1859b7e39190bdbaf2b445443ba7abbcc36a6f7b
thehonoredone672 <thehonoredone672@gmail.com>
2025-11-29 05:48:42 +0530

    Initial commit: Create initial version of the webpage

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add your custom scripts here
+console.log("Welcome to Dexys!");
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
